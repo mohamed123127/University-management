1c341096e337a6bb66fffdc9baeafa5c525497d8
NaZiM09 <cbanazim@gmail.com>
2025-05-19 10:07:11 +0100

    ....

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // أضف هذا
 import App from './components/App';
-import './index.css';
-import './config/i18n';
+import './index.css'
+import './config/i18n'; // Import i18n configuration
 import Problems from 'pages/test';
-import Attestation2 from 'pdfGenerator/attestation2';
-import SoutenanceMFE from 'pdfGenerator/SoutenanceMFE';
-import AttestationNonRéinscription from 'pdfGenerator/AttestationNonRéinscription';
-import AttestationAbandonDesEtudes from 'pdfGenerator/AttestationAbandonDesEtudes';
-import ConventionDePreStage from 'pdfGenerator/conventionDePreStage';
-import RecommandationStageDintégration from 'pdfGenerator/stageIntgration';
-import ProlongationStage from 'pdfGenerator/prolongationStage';
-import FicheDePresenceDeStage from 'pdfGenerator/ficheDePresenceDeStage';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* هذا هو الحل */}
-      <FicheDePresenceDeStage/>
-    </BrowserRouter>
+ <App/>
   </React.StrictMode>
 );
